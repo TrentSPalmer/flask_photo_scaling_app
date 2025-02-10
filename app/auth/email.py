4b67a96f567852711fdc2050236a7ed096df9612
Trent Palmer <trenttdually@gmail.com>
2025-02-09 17:03:05 -0800

    fix style app/auth/email.py

--- app/auth/email.py
+++ app/auth/email.py
@@ -8,13 +8,23 @@ from threading import Thread
 
 def send_password_reset_email(contributor, external_url):
     token = contributor.get_reset_password_token()
-    send_email('Photo App Reset Your Password',
-               sender=current_app.config['MAIL_ADMINS'][0],
-               recipients=[contributor.email],
-               text_body=render_template('email/reset_password_email_text.txt',
-                                         contributor=contributor, token=token, external_url=external_url),
-               html_body=render_template('email/reset_password_email_html.html',
-                                         contributor=contributor, token=token, external_url=external_url))
+    send_email(
+        'Photo App Reset Your Password',
+        sender=current_app.config['MAIL_ADMINS'][0],
+        recipients=[contributor.email],
+        text_body=render_template(
+            'email/reset_password_email_text.txt',
+            contributor=contributor,
+            token=token,
+            external_url=external_url,
+        ),
+        html_body=render_template(
+            'email/reset_password_email_html.html',
+            contributor=contributor,
+            token=token,
+            external_url=external_url,
+        ),
+    )
 
 
 def send_async_email(app, msg):
@@ -26,4 +36,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+    Thread(
+        target=send_async_email,
+        args=(current_app._get_current_object(), msg),
+    ).start()
